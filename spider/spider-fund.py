7be06f0b064392dc0c0d24aae7a03f2e7f059bac
KenMET <ken_processor@outlook.com>
2020-02-26 00:39:09 +0800

    Test code python.org spider

--- spider/spider-fund.py
+++ spider/spider-fund.py
@@ -1,1 +1,19 @@
-#import
+import time
+import datetime
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+
+chrome_options=Options()
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')
+driver = webdriver.Chrome(chrome_options=chrome_options)
+driver.get("http://www.python.org")
+assert "Python" in driver.title
+elem = driver.find_element_by_name("q")
+elem.clear()
+elem.send_keys("kenmet")
+elem.send_keys(Keys.RETURN)
+print (driver.page_source)
+driver.close()
