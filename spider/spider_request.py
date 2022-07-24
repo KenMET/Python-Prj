1f9d1ca00446a40f9a39e1372779c1fcd0a172a6
KenMET <ken_processor@outlook.com>
2022-07-24 15:38:20 +0800

    support soup parse and transfer to mysql format

--- spider/spider_request.py
+++ spider/spider_request.py
@@ -1,12 +1,20 @@
-import time
-import datetime
+import os, sys
+import time, datetime
 import json
 import requests
 
-#all fund base data must return as a list
+from bs4 import BeautifulSoup
+
+py_dir = os.path.dirname(os.path.realpath(__file__))
+py_name = os.path.realpath(__file__)[len(py_dir)+1:-3]
+sys.path.append(r'%s/'%(py_dir))
+sys.path.append(r'%s/../common_api/xml_operator'%(py_dir))
+import xml_operator as xo
+
+
+#all fund base data must return as a dict
 def request_base(code):
-    url1 = 'http://fundf10.eastmoney.com/jjjz_%s.html'%(code)
-    print (url1)
+    url1 = 'http://fundf10.eastmoney.com/jbgk_%s.html'%(code)
 
     header = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
               'Accept-Encoding': 'gzip, deflate',
@@ -17,74 +25,78 @@ def request_base(code):
               'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
                       'AppleWebKit/537.36 (KHTML, like Gecko) '
                       'Chrome/86.0.4240.198 Safari/537.36',}
+
     response = requests.get(url=url1, headers=header)
     response.encoding = 'utf-8'
-    data_full = response.text
+    if (response.status_code != 200):
+        print ('Request Base error:[%d]'%(response.status_code))
+        return {}
 
-    base_dict = {
-                'Id':code,
-                'Name':'None',
-                'Date':'',
-                'NetValue':'1.000', 
-                'Amplitude':'0.00%',
-                'BuyStatus':True,
-                'SellStatus':True,
-                'BuyRate':'0.10%',}
+    soup = BeautifulSoup(response.text, 'html.parser')
+    main_div = soup.find('div', class_ = 'txt_cont')
+    table = main_div.find_all('th')
+    dup_child_dict = {}
+    temp_dict = {}
+    for key_index in table:
+        val_index = key_index.next_sibling
+        key = key_index.text
+        val = ''
+        internal_children = val_index.find_all(['th', 'td'])
+        if (len(internal_children) > 0):
+            dup_child_dict.update({key:val_index})
+        else:
+            val = val_index.text
+        temp_dict.update({key:val})
 
-    find_str = '<h4 class="title"><a href="http://fund.eastmoney.com/%s.html">'%(code)
-    name = data_full[data_full.find(find_str)+len(find_str):]
-    name = name[:name.find('</h4>')]
-    name = name[:name.find(' ')]
-    base_dict['Name'] = name
-    
-    current_net = data_full[data_full.find('盘中估算'):]
-    current_net = current_net[:current_net.find('</label>')]
-    find_str = '<span id="fund_gsz"'
-    NVA = current_net[current_net.find(find_str)+len(find_str):]
-    NVA = NVA[NVA.find('>')+1:]
-    NVA = NVA[:NVA.find('</span>')]
-    find_str = '<span id="fund_gszf"'
-    PER = current_net[current_net.find(find_str)+len(find_str):]
-    PER = PER[PER.find('>')+1:]
-    PER = PER[:PER.find('</span>')]
-    base_dict['NetValue'] = NVA
-    base_dict['Amplitude'] = PER
+    for key in dup_child_dict:
+        internal_children = dup_child_dict[key].find_all(['th', 'td'])
+        for tmp in internal_children:
+            tmp.clear()
+        val = dup_child_dict[key].text
+        temp_dict.update({key:val})
+    ####################################################
+    main_div = soup.find_all('div', class_ = 'boxitem w790')
+    for index in main_div:
+        key_index = index.find('label', class_ = 'left')
+        val_index = index.find('p')
+        if (type(val_index) == type(None) or type(key_index) == type(None)):
+            continue
+        key = key_index.text
+        val = val_index.text.replace(' ', '').replace('\r','').replace('\n','').strip()
+        temp_dict.update({key:val})
 
-    find_str = '单位净值（'
-    date = data_full[data_full.find(find_str)+len(find_str):]
-    date = date[:date.find('）：')]
-    base_dict['Date'] = str(datetime.datetime.now().year) + '-' + date
+    return transfer_base_to_mysql(temp_dict)
 
-    fund_state = data_full[data_full.find('交易状态'):]
-    fund_state = fund_state[:fund_state.find('</label>')]
-    buy_state = fund_state[fund_state.find('<span>'):fund_state.find('</span>')]
-    sell_state = fund_state[fund_state.rfind('<span>'):fund_state.rfind('</span>')]
-    if buy_state.find('暂停') >= 0:
-        base_dict['BuyStatus'] = False
-    if sell_state.find('暂停') >= 0:
-        base_dict['SellStatus'] = False
+#all fund base data must return as a list
+def request_daily(code):
+    url1 = 'http://fund.eastmoney.com/%s.html'%(code)
 
-    sourcerate = data_full[data_full.find('<b class="sourcerate">'):]
-    sourcerate = sourcerate[:sourcerate.find('</label>')]
-    sourcerate = sourcerate[sourcerate.find('<b>'):]
-    buy_rate = sourcerate[sourcerate.find('<b>')+3:sourcerate.find('</b>')]
-    base_dict['BuyRate'] = buy_rate
+    header = {'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+              'Accept-Encoding': 'gzip, deflate',
+              'Accept-Language': 'zh-CN,zh;q=0.9',
+              'Connection': 'keep-alive',
+              'Host': 'fundf10.eastmoney.com',
+              'Referer': 'http://fund.eastmoney.com/',
+              'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) '
+                      'Chrome/86.0.4240.198 Safari/537.36',}
 
-    for index in base_dict:
-        if type('string') == type(base_dict[index]):
-            base_dict[index] = base_dict[index].replace('\r','').replace('\n','').strip().rstrip()
+    response = requests.get(url=url1, headers=header)
+    response.encoding = 'utf-8'
+    if (response.status_code != 200):
+        print ('Request Daily error:[%d]'%(response.status_code))
+        return {}
 
-    #print(current_net)
-    #print(fund_state)
-    #print(sourcerate)
-    #print(base_dict)
-    return base_dict
+    soup = BeautifulSoup(response.text, 'html.parser')
+    main_div = soup.find('div', class_ = 'fundDetail-main')
+
+
+    return {}
 
 #all fund net data must return as a list
 def request_net(code, days):
     url1 = 'http://api.fund.eastmoney.com/f10/lsjz?callback=jQuery18308926516764027739_1607151282970&fundCode=%s&pageIndex=1&pageSize=%s&startDate=&endDate=&_=xxxxxx'%(code,days)
     url2 = url1.replace('xxxxxx', str(int(round(time.time() * 1000))))
-    print (url2)
 
     header = {'Accept': '*/*',
               'Accept-Encoding': 'gzip, deflate',
@@ -97,17 +109,70 @@ def request_net(code, days):
                       'Chrome/86.0.4240.198 Safari/537.36',}
     response = requests.get(url=url2, headers=header)
     response.encoding = 'utf-8'
+    if (response.status_code != 200):
+        print ('Request Net error:[%d]'%(response.status_code))
+        return []
     data_full = response.text
-    data_json = data_full[data_full.find('(')+1 : data_full.find(')')]  #去掉头，剩下就是json格式数据
+    data_json = data_full[data_full.find('(')+1 : data_full.rfind(')')]  # remove head, the rest is the json data
     data_dict = json.loads(data_json)
     data_fund = data_dict['Data']
     data_list = data_fund['LSJZList']
-    return data_list
+    temp_list = []
+    for index in data_list:
+        temp_list.append(transfer_net_to_mysql(index))
+    return temp_list
 
-def main():
-    print (request_net('001629', 8))
-    print (request_base('001629'))
 
+def transfer_base_to_mysql(rq_dict):
+        map_dict = {
+            '基金简称':'Name',
+            '基金代码':'ID',
+            '基金类型':'Type',
+            '发行日期':'PublishDate',
+            '成立日期/规模':'EstablishmentDate_Size',
+            '资产规模':'AssetSize',
+            '份额规模':'ShareSize',
+            '基金管理人':'Manager',
+            '基金托管人':'Custodian',
+            '基金经理人':'ManagerPerson',
+            '成立来分红':'EstablishmentDividend',
+            '管理费率':'ManagementFeeRate',
+            '托管费率':'HostingFees',
+            '销售服务费率':'SalesServiceRate',
+            '最高认购费率':'MaximumSubscriptionRate',
+            '最高申购费率':'MaximumApplyRate',
+            '最高赎回费率':'MaximumRedemptionRate',
+            '业绩比较基准':'PerformanceComparisonBase',
+            '跟踪标的':'TargetTrack',
+            '投资目标':'InvestmentObjectives',
+            '投资理念':'InvestmentPhilosophy',
+            '投资范围':'InvestmentScope',
+            '投资策略':'InvestmentStrategy',
+            '分红政策':'DividendPolicy',
+            '风险收益特征':'RiskReturnCharacteristics',
+        }
+        temp_dict = {'Reserve':''}
+        for key in map_dict:
+            temp_dict.update({map_dict[key]:rq_dict.get(key, '')})
+        return temp_dict
+
+def transfer_net_to_mysql(rq_dict):
+        map_dict = {
+            'FSRQ': 'NetValueDate', 
+            'DWJZ': 'NetValueUnit', 
+            'LJJZ': 'NetValueCumulative', 
+            'JZZZL': 'DayGrowth', 
+            'SGZT': 'SubscriptionStatus', 
+            'SHZT': 'RedemptionStatus', 
+            'FHSP': 'DividendsSending',
+        }
+        temp_dict = {'Reserve':''}
+        for key in map_dict:
+            temp_dict.update({map_dict[key]:rq_dict.get(key, '')})
+        return temp_dict
 
 if __name__ == '__main__':
-    main()
+    print (request_net('001593', 2))
+    temp = request_base('161028')
+    for index in temp:
+        print ('[%s]:%s'%(index, temp[index]))
