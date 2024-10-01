15ab8d16443bd92902e59610656c70cf9485efc2
Kanos <ken_processor@outlook.com>
2024-10-02 00:38:51 +0800

    [Dream] - Append new spider for dog&cat 1. Fetch full history supported 2. A new format for dog database

---
+++ dream/input/dog_fetch.py
@@ -0,0 +1,176 @@
+#!/bin/python3
+
+# System lib
+import os
+import sys
+import json
+import random
+import logging
+import hashlib
+import datetime, time
+
+# Customsized lib
+py_dir = os.path.dirname(os.path.realpath(__file__))
+py_name = os.path.realpath(__file__)[len(py_dir)+1:-3]
+sys.path.append(r'%s/'%(py_dir))
+import akshare as ak
+import adata
+import pandas as pd
+sys.path.append(r'%s/../../mysql'%(py_dir))
+import db_cat as dbc
+import db_dream_dog as dbdd
+sys.path.append(r'%s/../../common_api/xml_operator'%(py_dir))
+import xml_operator as xo
+
+logger = None
+def get_logger():
+    global logger
+    return logger
+def set_logger(logger_tmp):
+    global logger
+    logger = logger_tmp
+def logger_init(log_name=py_name):
+    logger_tmp = logging.getLogger()
+    logger_tmp.setLevel(logging.INFO)
+    log_file = py_dir + '/' + log_name + '.log'
+    # file_handler = logging.FileHandler(log_file, mode='a')  # Continue writing the file
+    file_handler = logging.FileHandler(log_file, mode='w')  # start over writing the file
+    file_handler.setLevel(logging.INFO)
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+    file_handler.setFormatter(formatter)
+    console_handler.setFormatter(formatter)
+    logger_tmp.addHandler(file_handler)
+    logger_tmp.addHandler(console_handler)
+    #logger_tmp.info('Logger Creat Success')
+    #logger.debug("this is debug") # Enable to modify fh.setLevel(logging.INFO) to logging.DEDBUG
+    #logger.warning("this is warning")
+    #logging.error("this is error")
+    set_logger(logger_tmp)
+
+def get_config_dict():
+    file_name = '%s/config.xml'%(py_dir)
+    cfg = xo.operator(file_name)
+    cfg_dict = cfg.walk_node(cfg.root_node)
+    return cfg_dict
+
+def get_cat_list_from_config():     # From config file(default)
+    cfg_dict = get_config_dict()
+    cat_list = cfg_dict.get('cat_list', {}).get('id', [])
+    if type(cat_list) == type(''):
+        cat_list = [cat_list, ]
+    return cat_list
+
+def get_cat_list_from_db():         # From cat_survey
+    pass
+
+def get_dog_list_from_config():     # From config file(extra_list)
+    cfg_dict = get_config_dict()
+    dog_list = cfg_dict.get('dog_list', {}).get('id', [])
+    if type(dog_list) == type(''):
+        dog_list = [dog_list, ]
+    return dog_list
+
+def get_dog_list_from_db():         # From cat holding
+    def get_last_quarter(offset=0):
+        today = datetime.datetime.now().date()
+        quarter_ends = [(3, 31), (6, 30), (9, 30), (12, 31)]
+        current_quarter = (today.month - 1) // 3 + 1
+        target_quarter = (current_quarter + offset - 1) % 4
+        target_year = today.year + (current_quarter + offset - 1) // 4
+        return datetime.date(target_year, *quarter_ends[target_quarter])
+
+    dog_code_list = []
+    cat_list = get_cat_list_from_config()
+    db = dbc.catdb('kanos_cat')
+    for cat_index in cat_list:
+        dog_temp_list = db.queryCatHoldingByQuarter(cat_index, get_last_quarter())
+        if len(dog_temp_list) == 0:
+            dog_temp_list = db.queryCatHoldingByQuarter(cat_index, get_last_quarter(-1))
+            if len(dog_temp_list) == 0:
+                dog_temp_list = db.queryCatHoldingByQuarter(cat_index, get_last_quarter(-2))
+        for index in dog_temp_list:
+            dog_id = index.DogCodeQuarter[index.DogCodeQuarter.find(':')+1:]
+            if (dog_id not in dog_code_list):
+                dog_code_list.append(dog_id)
+    db.closeSession()
+    return dog_code_list
+
+def get_market(dog_code: str) -> str:
+    if dog_code.startswith(('5', '6')):
+        return 'sh'
+    elif dog_code.startswith(('0', '1', '3')):
+        return 'sz'
+    elif dog_code.startswith('4'):
+        return 'bj'
+    else:
+        return None
+
+# date   open  close   high    low     amount
+def get_dog_daily_hist(dog_id, **kwargs):
+    try:
+        df = ak.stock_zh_a_hist(symbol=dog_id, period="daily", adjust="qfq", **kwargs)
+        df.drop(columns=['股票代码', '成交量', '振幅', '涨跌幅', '涨跌额', '换手率'], inplace=True)
+        df.rename(columns={'日期': 'Date', '开盘': 'Open'}, inplace=True) # Replace title
+        df.rename(columns={'收盘': 'Close', '最高': 'High'}, inplace=True) # Replace title
+        df.rename(columns={'最低': 'Low', '成交额': 'Amount'}, inplace=True) # Replace title
+        return df
+    except Exception as e:
+        try:
+            df = ak.stock_zh_a_daily(symbol=get_market(dog_id)+dog_id, adjust="qfq", **kwargs)
+            df.drop(columns=['volume', 'outstanding_share', 'turnover'], inplace=True)
+            df.columns = df.columns.str.title()
+            return df
+        except Exception as e:
+            try:
+                df = ak.stock_zh_a_hist_tx(symbol=get_market(dog_id)+dog_id, adjust="qfq", **kwargs)
+                df.columns = df.columns.str.title()
+                df['Amount'] = ((df['Open'] + df['Close'] + df['High'] + df['Low']) / 4) * df['Amount'] * 100
+                return df
+            except Exception as e:
+                return None
+
+def get_capital_flow(dog_id):
+    df = ak.stock_individual_fund_flow(stock="159509", market=get_market("159509"))
+    df.drop(columns=['收盘价', '涨跌幅'], inplace=True)
+    df = df.drop(columns=df.filter(like='净占比').columns)
+    df.rename(columns={'主力净流入-净额': 'Inflow_Main', '超大单净流入-净额': 'Inflow_Max'}, inplace=True) # Replace title
+    df.rename(columns={'大单净流入-净额':'Inflow_Lg', '中单净流入-净额': 'Inflow_Mid'}, inplace=True) # Replace title
+    df.rename(columns={'小单净流入-净额': 'Inflow_Sm', '日期': 'Date'}, inplace=True) # Replace title
+    return df
+
+def main():
+    logger_init()
+    get_logger().info('Logger Creat Success')
+
+    # Start fetch dog
+    dog_list = get_dog_list_from_db()
+    dog_list = list(set(dog_list).union(get_dog_list_from_config()))
+
+    db = dbdd.dogdb('dream_dog')
+    for dog_index in dog_list:
+        if (not db.is_table_exist(dog_index)):      # New a table to insert
+            get_logger().info('Dog not exist, new a table...')
+            db.create_dog_market_table(dog_index)
+            df1 = get_dog_daily_hist(dog_index)
+            df2 = get_capital_flow(dog_index)
+            df = pd.merge(df1, df2, on='Date', how='left')
+            df.fillna(0, inplace=True)
+        else:
+            current_date = datetime.datetime.now().strftime('%Y%m%d')
+            start_date = (datetime.datetime.now() - datetime.timedelta(days=15)).strftime('%Y%m%d')    # 15 days ago
+            df1 = get_dog_daily_hist(dog_index, start_date=start_date, end_date=current_date)
+            df2 = get_capital_flow(dog_index)
+            df = pd.merge(df1, df2, on='Date', how='left')
+            df.fillna(0, inplace=True)
+        dog_update_list = df.to_dict(orient='records')
+        get_logger().info('Start insert for [%s]'%(dog_index))
+        for dog_update_index in dog_update_list:
+            #get_logger().info(dog_update_index)
+            flag = db.insert_dog_market(dog_index, dog_update_index)
+            if (not flag):
+                get_logger().info('dog insert failed: %s'%(str(dog_update_index)))
+
+if __name__ == '__main__':
+    main()
---
+++ mysql/db_dream_dog.py
@@ -0,0 +1,135 @@
+#!/bin/python3
+
+# System lib
+import os, sys
+import time, datetime
+from sqlalchemy import MetaData, Table, desc
+from sqlalchemy import Column, Integer, String, DATETIME, DATE, Text, VARCHAR, JSON
+
+# Customsized lib
+import db_base as dbb
+
+class dogdb(dbb.basedb):
+
+############# Dog Market Start ########################
+    def create_dog_market_class(self, dog_id):
+        table_name = 'dog_market_%s'%(dog_id)
+        if table_name not in self.db_class:
+            new_class = type(table_name, (self.Base, ), dict(
+                __tablename__ = table_name,
+                Date = Column(DATE, primary_key=True),
+                Open = Column(Text, nullable=True),
+                Close = Column(Text, nullable=True),
+                High = Column(Text, nullable=True),
+                Low = Column(Text, nullable=True),
+                Amount = Column(Text, nullable=True),
+                Inflow_Main = Column(Text, nullable=True),
+                Inflow_Max = Column(Text, nullable=True),
+                Inflow_Lg = Column(Text, nullable=True),
+                Inflow_Mid = Column(Text, nullable=True),
+                Inflow_Sm = Column(Text, nullable=True),
+                Reserved = Column(Text, nullable=True),
+            ))
+            self.db_class.update({table_name:new_class})
+        return self.db_class[table_name]
+
+    def create_dog_market_table(self, dog_id):
+        meta = MetaData()
+        tmp = Table(
+            'dog_market_%s'%(dog_id), meta,
+            # Primary key
+            Column('Date', DATE, primary_key=True),
+            # Other keys
+            Column('Open', Text, nullable=True),
+            Column('Close', Text, nullable=True),
+            Column('High', Text, nullable=True),
+            Column('Low', Text, nullable=True),
+            Column('Amount', Text, nullable=True),
+            Column('Inflow_Main', Text, nullable=True),
+            Column('Inflow_Max', Text, nullable=True),
+            Column('Inflow_Lg', Text, nullable=True),
+            Column('Inflow_Mid', Text, nullable=True),
+            Column('Inflow_Sm', Text, nullable=True),
+            Column('Reserved', Text, nullable=True),
+        )
+        meta.create_all(self.engine)
+
+    def is_table_exist(self, dog_id):
+        tables = self.queryTable()
+        if ('dog_market_%s'%(dog_id) not in tables):
+            return False
+        return True
+
+    def is_date_exist(self, dog_id, date):
+        ret = self.query_dog_markey_by_date(dog_id, date)
+        if len(ret) > 0:
+            return True
+        return False
+
+    def query_dog_markey_all(self, dog_id):
+        if self.session is None:
+            self.connectdb()
+        dog_market = self.create_dog_market_class(dog_id)
+        result = self.session.query(dog_market).all()
+        try:
+            self.session.commit()
+        except:
+            return []
+        else:
+            return result
+
+    def query_dog_markey_last(self, dog_id):
+        if self.session is None:
+            self.connectdb()
+        dog_market = self.create_dog_market_class(dog_id)
+        result = self.session.query(dog_market).order_by(desc(dog_market.Date)).first()
+        try:
+            self.session.commit()
+        except:
+            return []
+        else:
+            return result
+
+    def query_dog_markey_by_date(self, dog_id, date):
+        if self.session is None:
+            self.connectdb()
+        dog_market = self.create_dog_market_class(dog_id)
+        result = self.session.query(dog_market).filter(dog_market.Date == date).all()
+        try:
+            self.session.commit()
+        except:
+            return []
+        else:
+            return result
+
+    def updateDogMoneyFlowByDate(self, dog_id, date, dog_dict):
+        if self.session is None:
+            self.connectdb()
+        if (self.is_date_exist(dog_id, date)):
+            dog_market = self.create_dog_market_class(dog_id)
+            self.session.query(dog_market).filter(dog_market.Date == date).update(dog_dict)
+            try:
+                self.session.commit()
+            except:
+                return False
+            else:
+                return True
+        else:
+            return self.insertDogMoneyFlow(dog_id, dog_dict)
+
+    def insert_dog_market(self, dog_id, dog_dict):
+        if self.session is None:
+            self.connectdb()
+        if (self.is_date_exist(dog_id, dog_dict['Date'])):      # Skip if exist
+            return False
+        dog_market = self.create_dog_market_class(dog_id)
+        dog = dog_market()
+        dog = self.get_obj_from_dict(dog_dict, dog)
+        try:
+            self.session.add(dog)
+            self.session.commit()
+        except:
+            return False
+        else:
+            return True
+############# Dog Market End ########################
