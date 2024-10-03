c5d71ec3521affc30e9f2b8f48cf2a59d1d30fc8
Kanos <ken_processor@outlook.com>
2024-10-03 14:52:01 +0800

    [Dream] - Supported US dog market fetch 1. Append US dog to config 2. Supported paramentors input (cn_a/us)

--- dream/input/dog_fetch.py
+++ dream/input/dog_fetch.py
@@ -7,6 +7,7 @@ import json
 import random
 import logging
 import hashlib
+import argparse
 import datetime, time
 
 # Customsized lib
@@ -19,6 +20,7 @@ import pandas as pd
 sys.path.append(r'%s/../../mysql'%(py_dir))
 import db_cat as dbc
 import db_dream_dog as dbdd
+import db_dream_dog_info as dbddi
 sys.path.append(r'%s/../../common_api/xml_operator'%(py_dir))
 import xml_operator as xo
 
@@ -65,9 +67,9 @@ def get_cat_list_from_config():     # From config file(default)
 def get_cat_list_from_db():         # From cat_survey
     pass
 
-def get_dog_list_from_config():     # From config file(extra_list)
+def get_dog_list_from_config(market):     # From config file(extra_list)
     cfg_dict = get_config_dict()
-    dog_list = cfg_dict.get('dog_list', {}).get('id', [])
+    dog_list = cfg_dict.get('dog_%s_list'%(market), {}).get('id', [])
     if type(dog_list) == type(''):
         dog_list = [dog_list, ]
     return dog_list
@@ -107,8 +109,21 @@ def get_market(dog_code: str) -> str:
     else:
         return None
 
+def get_us_fullcode(dod_id):
+    db_info = dbddi.db('dream_dog')
+    res = db_info.query_dog_fullcode_by_code(args.market, dod_id)
+    if len(res) == 0:
+        get_logger().error('Dog not found [%s]'%(dod_id))
+        return '' 
+    elif len(res) != 1:
+        tmp_list = [i.Code for i in res]
+        dog_full_code = [item for item in tmp_list if item.endswith('.' + dod_id)][0]
+    else:
+        dog_full_code = res[0].Code
+    return dog_full_code
+
 # date   open  close   high    low     amount
-def get_dog_daily_hist(dog_id, **kwargs):
+def get_dog_cn_a_daily_hist(dog_id, **kwargs):
     try:
         df = ak.stock_zh_a_hist(symbol=dog_id, period="daily", adjust="qfq", **kwargs)
         df.drop(columns=['股票代码', '成交量', '振幅', '涨跌幅', '涨跌额', '换手率'], inplace=True)
@@ -129,10 +144,11 @@ def get_dog_daily_hist(dog_id, **kwargs):
                 df['Amount'] = ((df['Open'] + df['Close'] + df['High'] + df['Low']) / 4) * df['Amount'] * 100
                 return df
             except Exception as e:
+                get_logger().info('Dog[%s] fetch failed...'%(dog_id))
                 return None
 
-def get_capital_flow(dog_id):
-    df = ak.stock_individual_fund_flow(stock="159509", market=get_market("159509"))
+def get_dog_cn_a_capital_flow(dog_id):
+    df = ak.stock_individual_fund_flow(stock=dog_id, market=get_market(dog_id))
     df.drop(columns=['收盘价', '涨跌幅'], inplace=True)
     df = df.drop(columns=df.filter(like='净占比').columns)
     df.rename(columns={'主力净流入-净额': 'Inflow_Main', '超大单净流入-净额': 'Inflow_Max'}, inplace=True) # Replace title
@@ -140,30 +156,59 @@ def get_capital_flow(dog_id):
     df.rename(columns={'小单净流入-净额': 'Inflow_Sm', '日期': 'Date'}, inplace=True) # Replace title
     return df
 
-def main():
+def get_dog_us_daily_hist(dog_id, **kwargs): 
+    try:
+        df = ak.stock_us_hist(symbol=dog_id, period="daily", adjust="qfq", **kwargs)
+        df.drop(columns=['成交量', '振幅', '涨跌幅', '涨跌额', '换手率'], inplace=True)
+        df.rename(columns={'日期': 'Date', '开盘': 'Open'}, inplace=True) # Replace title
+        df.rename(columns={'收盘': 'Close', '最高': 'High'}, inplace=True) # Replace title
+        df.rename(columns={'最低': 'Low', '成交额': 'Amount'}, inplace=True) # Replace title
+        return df
+    except Exception as e:
+        get_logger().info('Dog[%s] fetch failed...'%(dog_id))
+        return None
+
+
+def main(args):
     logger_init()
     get_logger().info('Logger Creat Success')
 
-    # Start fetch dog
-    dog_list = get_dog_list_from_db()
-    dog_list = list(set(dog_list).union(get_dog_list_from_config()))
+    dog_list = []
+    if (args.market == 'us'):
+        dog_list = get_dog_list_from_config(args.market)
+        get_logger().info(dog_list)
+    elif (args.market == 'cn_a'):
+        dog_list = get_dog_list_from_db()
+        dog_list = list(set(dog_list).union(get_dog_list_from_config(args.market)))
 
     db = dbdd.db('dream_dog')
     for dog_index in dog_list:
         if (not db.is_table_exist(dog_index)):      # New a table to insert
-            get_logger().info('Dog not exist, new a table...')
+            get_logger().info('Dog[%s] not exist, new a table...'%(dog_index))
             db.create_dog_market_table(dog_index)
-            df1 = get_dog_daily_hist(dog_index)
-            df2 = get_capital_flow(dog_index)
-            df = pd.merge(df1, df2, on='Date', how='left')
-            df.fillna(0, inplace=True)
+            if (args.market == 'us'):
+                dog_full_code = get_us_fullcode(dog_index)
+                if dog_full_code == '':
+                    continue
+                df = get_dog_us_daily_hist(dog_full_code)
+            elif (args.market == 'cn_a'):
+                df1 = get_dog_cn_a_daily_hist(dog_index)
+                df2 = get_dog_cn_a_capital_flow(dog_index)
+                df = pd.merge(df1, df2, on='Date', how='left')
+                df.fillna(0, inplace=True)
         else:
             current_date = datetime.datetime.now().strftime('%Y%m%d')
-            start_date = (datetime.datetime.now() - datetime.timedelta(days=15)).strftime('%Y%m%d')    # 15 days ago
-            df1 = get_dog_daily_hist(dog_index, start_date=start_date, end_date=current_date)
-            df2 = get_capital_flow(dog_index)
-            df = pd.merge(df1, df2, on='Date', how='left')
-            df.fillna(0, inplace=True)
+            start_date = (datetime.datetime.now() - datetime.timedelta(days=10)).strftime('%Y%m%d')    # 10 days ago
+            if (args.market == 'us'):
+                dog_full_code = get_us_fullcode(dog_index)
+                if dog_full_code == '':
+                    continue
+                df = get_dog_us_daily_hist(dog_full_code, start_date=start_date, end_date=current_date)
+            elif (args.market == 'cn_a'):
+                df1 = get_dog_cn_a_daily_hist(dog_index, start_date=start_date, end_date=current_date)
+                df2 = get_dog_cn_a_capital_flow(dog_index)
+                df = pd.merge(df1, df2, on='Date', how='left')
+                df.fillna(0, inplace=True)
         dog_update_list = df.to_dict(orient='records')
         get_logger().info('Start insert for [%s]'%(dog_index))
         for dog_update_index in dog_update_list:
@@ -173,4 +218,12 @@ def main():
                 get_logger().info('dog insert failed: %s'%(str(dog_update_index)))
 
 if __name__ == '__main__':
-    main()
+    # Create ArgumentParser Object
+    parser = argparse.ArgumentParser(description="A input module for dog info fetch")
+    
+    # Append arguments
+    parser.add_argument('--market', type=str, default='cn_a', help='Now supported: "cn_a"(default),"us"')
+    
+    # 解析命令行参数
+    args = parser.parse_args()
+    main(args)
